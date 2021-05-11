d6a5cfb60c8713d2f45f831a2793718fd90a3920
Laura Jacquin <laura.jcqn@live.fr>
2021-05-11 11:31:28 +0200

    change swipeLabel text and arrowImage image in landscape and portrait mode

--- Instagrid/Instagrid/Controller/ViewController.swift
+++ Instagrid/Instagrid/Controller/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var arrowImage: UIImageView!
+    @IBOutlet weak var swipeLabel: UILabel!
+    @IBOutlet weak var layoutOneButton: UIButton!
+    @IBOutlet weak var layoutTwoButton: UIButton!
+    @IBOutlet weak var layoutThreeButton: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        orientationChange()
     }
 
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        orientationChange()
+    }
+    
+    private func orientationChange() {
+        if UIDevice.current.orientation.isLandscape {
+            arrowImage.image = #imageLiteral(resourceName: "Arrow Left")
+            swipeLabel.text = "Swipe left to share"
+        }
+        else {
+            arrowImage.image = #imageLiteral(resourceName: "Arrow Up")
+            swipeLabel.text = "Swipe up to share"
+        }
+    }
 
 }
 
