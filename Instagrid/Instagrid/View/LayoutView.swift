61ebb04add5575d7ac5f259882b13fda07841bb7
Laura Jacquin <laura.jcqn@live.fr>
2021-05-12 11:26:00 +0200

    add enum Style in LayoutView

---
+++ Instagrid/Instagrid/View/LayoutView.swift
@@ -0,0 +1,37 @@
+
+
+import UIKit
+
+class LayoutView: UIView {
+
+    @IBOutlet var layout1: UIView!
+    @IBOutlet var layout2: UIView!
+    @IBOutlet var layout3: UIView!
+    
+    enum Style {
+        case layout1, layout2, layout3
+    }
+    
+    var style: Style = .layout1 {
+        didSet {
+            setStyle(style)
+        }
+    }
+    private func setStyle(_ style: Style) {
+        switch style {
+        case .layout1:
+            layout1.isHidden = false
+            layout2.isHidden = true
+            layout3.isHidden = true
+        case .layout2:
+            layout1.isHidden = true
+            layout2.isHidden = false
+            layout3.isHidden = true
+        case .layout3:
+            layout1.isHidden = true
+            layout2.isHidden = true
+            layout3.isHidden = false
+        }
+    }
+
+}
