598064f8b0c7d6cb328df1dce7a8c5815054b83a
Laura Jacquin <laura.jcqn@live.fr>
2021-05-17 12:46:55 +0200

    add comments on functions for pictures picking

--- Instagrid/Instagrid/Controller/ViewController.swift
+++ Instagrid/Instagrid/Controller/ViewController.swift
@@ -12,7 +12,8 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     @IBOutlet weak var layoutThreeSelected: UIImageView!
     @IBOutlet weak var layoutView: LayoutView!
     
-    var tmpButton: UIButton?
+    // on créé une variable selectedButton qui est un UIButton et qui représente le bouton sur lequel on a appuyé :
+    var selectedButton: UIButton?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,13 +36,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         }
     }
     
-    @IBAction func buttonTapped(_ sender: UIButton) {
-        tmpButton = sender
-        let picker = UIImagePickerController()
-        picker.delegate = self
-        self.present(picker, animated: true)
-    }
-    
+   
     @IBAction func selectLayout(_ sender: UIButton) {
         switch sender {
         case layoutOneButton:
@@ -64,13 +59,31 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         }
     }
     
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        // on précise que le sender est le bouton qui vient d'être tapé :
+        selectedButton = sender
+        // on créé une variable picker qui est un UIImagePickerController :
+        let picker = UIImagePickerController()
+        // on précise que le delegate du picker est bien notre ViewController :
+        picker.delegate = self
+        // on présente sur la page le picker, et on active l'animation de son affichage :
+        self.present(picker, animated: true)
+    }
     
+    // MARK: - UIImagePickerControllerDelegate
+    
+    // fonction du protocole appelée par le picker qui prend en paramètre le picker et un dictionnaire clé/valeur :
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        // on créé une variable image dans laquelle on déballe l'image choisie par l'utilisateur
+        // s'il y a bien une image :
         if let image = info[.originalImage] as? UIImage {
-            tmpButton?.setBackgroundImage(image, for: .normal)
-            tmpButton = nil
+            // on set l'image selectionnée sur le bouton pour qu'il l'affiche en état normal :
+            selectedButton?.setBackgroundImage(image, for: .normal)
+            // on repasse la valeur de selectedButton a nil pour éviter les erreurs sur un bouton
+            // précédemment séléctionné :
+            selectedButton = nil
+            // on enlève le picker avec son animation de dismiss une fois l'image affichée :
             picker.dismiss(animated: true)
         }
     }
 }
-
