c152fc0e40a39904e1fc88eeb2f104da330952a0
Laura Jacquin <laura.jcqn@live.fr>
2022-04-04 16:58:50 +0200

    refacto ViewController: added markdowns, extension, rearranged code, translated comments in english and added an alert when user is done with sharing their image

--- Instagrid/Instagrid/Instagrid/Controller/ViewController.swift
+++ Instagrid/Instagrid/Instagrid/Controller/ViewController.swift
@@ -2,15 +2,17 @@
 import UIKit
 import Photos
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ViewController: UIViewController {
     
     // MARK: - Outlets
+
     @IBOutlet weak var arrowImage: UIImageView!
     @IBOutlet weak var swipeLabel: UILabel!
     @IBOutlet weak var layoutView: LayoutView!
     @IBOutlet weak var layoutOptions: LayoutOptionView!
-    
-    // on créé une variable selectedButton qui est un UIButton et qui représente le bouton sur lequel on a appuyé :
+
+    // MARK: - Property
+
     private var selectedButton: UIButton?
     
     
@@ -20,41 +22,46 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         super.viewDidLoad()
         orientationChange()
         setDefaultStyle()
-        // on créé le geste Swipe Up en précisant la target, et l'action à réaliser,
-        // puis la direction du swipe, et on ajoute le geste :
-        let swipeUp = UISwipeGestureRecognizer(target: self, action: #selector(swipeUp(_:)))
-        swipeUp.direction = .up
-        layoutView.addGestureRecognizer(swipeUp)
-        // pareil pour Swipe Left :
-        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(swipeLeft(_:)))
-        swipeLeft.direction = .left
-        layoutView.addGestureRecognizer(swipeLeft)
-        // on reçoit ici la notification :
+        createSwipeUpWithActionAndDirection()
+        createSwipeLeftWithActionAndDirection()
+
+        // Notifications observer:
         let name = Notification.Name(rawValue: "LayoutStyle")
         NotificationCenter.default.addObserver(self, selector: #selector(selectLayout(_:)), name: name, object: nil)
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-         // on demande l'autorisation a l'utilisateur d'acceder a ses photos
+         // ask user authorization to access their library:
         PHPhotoLibrary.requestAuthorization { (status) in
-            // on ne fait rien avec cette autorisation pour le moment.
+            // nothing is done with it for now.
         }
     }
-    
-    // fonction qui permet d'adapter les éléments d'interface suivant l'orientation de l'appareil :
+
+    // allows to adapt interface elements style with device orientation:
     override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
         super.viewWillTransition(to: size, with: coordinator)
         orientationChange()
     }
-    
-    
-    // MARK: - Actions
-    
 
-    // Fonction appelée quand on reçoit la notification émise du LayoutOptionView,
-    // qui va permettre d'afficher le layout selon le style choisi grâce à un switch :
-    @objc func selectLayout(_ notification: Notification) {
+    // MARK: - Action
+
+    // all buttons are linked to this action:
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        // selected button takes the sender as its value:
+        selectedButton = sender
+        // create a UIImagePickerController:
+        let picker = UIImagePickerController()
+        // declare ViewController as picker's delegate :
+        picker.delegate = self
+        // display picker:
+        self.present(picker, animated: true)
+    }
+
+    // MARK: - Private
+
+    // called when notification is observed, allows to display the correct layout from chosen style with a switch:
+    @objc private func selectLayout(_ notification: Notification) {
         if let style = notification.userInfo?["style"] as? String {
             switch style {
             case "layout1":
@@ -68,77 +75,12 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             }
         }
     }
-    
-    // tous les boutons font la même chose, ils sont tous reliés à cette action :
-    @IBAction func buttonTapped(_ sender: UIButton) {
-        // on précise que le sender est le bouton qui vient d'être tapé :
-        selectedButton = sender
-        // on créé une variable picker qui est un UIImagePickerController :
-        let picker = UIImagePickerController()
-        // on précise que le delegate du picker est bien notre ViewController avec self :
-        picker.delegate = self
-        // on présente sur la page le picker, et on active l'animation de son affichage :
-        self.present(picker, animated: true)
-    }
-    
-    @objc func swipeUp(_ sender: UISwipeGestureRecognizer) {
-        if UIDevice.current.orientation.isPortrait {
-            UIView.animate(withDuration: 0.5) {
-                // le résultat du calcul pour faire disparaitre la LayoutView :
-                // La translation en y = la taille de l'ecran divisée par 2 + la taille de la vue divisée par 2
-                // la translation s'applique a partir du centre de layoutView.
-                let translationY = -(self.view.bounds.height/2 + self.layoutView.bounds.height/2)
-                // on applique la translation :
-                self.layoutView.transform = CGAffineTransform(translationX: 0, y: translationY)
-            } completion: { (success) in
-                self.shareImage()
-            }
-        }
-    }
-    
-    @objc func swipeLeft(_ sender: UISwipeGestureRecognizer) {
-        if UIDevice.current.orientation.isLandscape {
-            UIView.animate(withDuration: 0.5) {
-                // le résultat du calcul avec la translation en x cette fois pour faire disparaitre la LayoutView :
-                let translationX = -(self.view.bounds.width/2 + self.layoutView.bounds.width/2)
-                // on applique la translation :
-                self.layoutView.transform = CGAffineTransform(translationX: translationX, y: 0)
-            } completion: { (success) in
-                self.shareImage()
-            }
-        }
-    }
-    
-    
-    // MARK: - UIImagePickerControllerDelegate
-    
-    // fonction du protocole appelée par le picker qui prend en paramètre le picker et un dictionnaire clé/valeur :
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        // on créé une variable image dans laquelle on déballe l'image choisie par l'utilisateur
-        // s'il y a bien une image :
-        if let image = info[.originalImage] as? UIImage {
-            // on change le mode d'affichage de l'image du bouton en scaleAspectFill
-            selectedButton?.imageView?.contentMode = .scaleAspectFill
-            // on set l'image selectionnée sur le bouton pour qu'il l'affiche en état normal :
-            selectedButton?.setImage(image, for: .normal)
-            // on repasse la valeur de selectedButton a nil pour éviter les erreurs sur un bouton
-            // précédemment séléctionné :
-            selectedButton = nil
-            // on enlève le picker avec son animation de dismiss :
-            picker.dismiss(animated: true)
-        }
-    }
-    
-    
-    // MARK: - Private func
-    
-    // le style par défaut :
+
     private func setDefaultStyle() {
         layoutView.setStyle(.layout1)
         layoutOptions?.setLayoutStyle(style: .layout1)
     }
-    
-    // fonction qui s'execute au changement d'orientation :
+
     private func orientationChange() {
         if UIDevice.current.orientation.isLandscape {
             arrowImage.image = #imageLiteral(resourceName: "Arrow Left")
@@ -149,29 +91,23 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             swipeLabel.text = "Swipe up to share"
         }
     }
-    
-    // fonction qui va permettre le partage sur les réseaux sociaux :
+
     private func shareImage() {
-        // on vérifie la permission d'enregistrer des images et
-        // on stocke le resultat true ou false dans canSaveImage :
-        let canSaveImage = checkAuthorizationStatus()
-        // si c'est false, on lance l'animation de retour et on sort avec return :
-        if canSaveImage == false {
+        // check authorization to save images:
+        guard checkAuthorizationStatus() else {
             reverseTranslation()
             return
         }
-        // on stocke déjà ce que l'on veut exporter :
+        // stock what should be exported:
         let activityItems = [exportImage()]
-        // puis on créé une instance de UIActivityViewController qui prend en paramètre
-        // ce que l'on souhaite partager, et les supports de partages désirés,
-        // ici à nil pour obtenir les options automatiques qui peuvent partager des images :
+        // instanciate UIActivityViewController with items to export in its parameters, appilcationActivities is nil to display default option that can share images:
         let activityController = UIActivityViewController(activityItems: activityItems, applicationActivities: nil)
-        // on précise ensuite dans une closure l'action à réaliser quand le partage a été effectué,
-        // ici l'animation de retour :
+        // add in a closure the return animation of the view to its initial position:
         activityController.completionWithItemsHandler = { activity, completed, items, error in
             self.reverseTranslation()
+            self.successAlert()
         }
-        // on l'affiche :
+        // and display:
         self.present(activityController, animated: true)
     }
     
@@ -188,32 +124,8 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             return false
         }
     }
-    
-    // alerte qui indique à l'utilisateur qu'on a besoin des droits :
-    private func accessAlert() {
-        let alertController = UIAlertController(title: "Warning", message: "We need access to your gallery", preferredStyle: .alert)
-        // on créé le bouton qui va ensuite masquer l'alerte et demander les autorisations :
-        alertController.addAction(UIAlertAction(title: "Ok", style: .default, handler: { (action) in
-            PHPhotoLibrary.requestAuthorization { (status) in }
-        }))
-        // on l'affiche :
-        present(alertController, animated: true, completion: nil)
-    }
-    
-    // alerte qui indique à l'utilisateur qu'il manque les autorisations nécessaires :
-    private func needAccessAlert() {
-        let alertController = UIAlertController(title: "Authorization denied", message: "Go to Settings > Instagrid and grant access to your photos to continue", preferredStyle: .alert)
-        // on créé le bouton qui va envoyer l'utilisateur dans ses paramètres et ensuite masquer l'alerte :
-        alertController.addAction(UIAlertAction(title: "Go to Settings", style: .default, handler: { (action) in
-            if let settingsURL = URL(string: UIApplication.openSettingsURLString) {
-                UIApplication.shared.open(settingsURL)
-            }
-        }))
-        // on l'affiche :
-        present(alertController, animated: true, completion: nil)
-    }
-    
-    // fonction qui permet de transformer notre layoutView en une image à exporter :
+
+    // allows to transform layoutView in a UIImage to export:
     private func exportImage() -> UIImage {
         let renderer = UIGraphicsImageRenderer(size: layoutView.bounds.size)
         let image = renderer.image { ctx in
@@ -222,10 +134,106 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         return image
     }
     
-    // fonction qui rend à la vue sa position initiale :
+
+
+    // MARK: - Gestures & Translations
+
+    private func createSwipeUpWithActionAndDirection() {
+        // create gesture with action:
+        let swipeUp = UISwipeGestureRecognizer(target: self, action: #selector(swipeUp(_:)))
+        // give direction of swipe:
+        swipeUp.direction = .up
+        // add gesture to the view:
+        layoutView.addGestureRecognizer(swipeUp)
+    }
+
+    private func createSwipeLeftWithActionAndDirection() {
+        // create gesture with action:
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(swipeLeft(_:)))
+        // give direction of swipe:
+        swipeLeft.direction = .left
+        // add gesture to the view:
+        layoutView.addGestureRecognizer(swipeLeft)
+    }
+
+    @objc private func swipeUp(_ sender: UISwipeGestureRecognizer) {
+        if UIDevice.current.orientation.isPortrait {
+            UIView.animate(withDuration: 0.5) {
+               // translation calculus:
+                let translationY = -(self.view.bounds.height/2 + self.layoutView.bounds.height/2)
+                // the translation itself:
+                self.layoutView.transform = CGAffineTransform(translationX: 0, y: translationY)
+            } completion: { (success) in
+                self.shareImage()
+            }
+        }
+    }
+
+    @objc private func swipeLeft(_ sender: UISwipeGestureRecognizer) {
+        if UIDevice.current.orientation.isLandscape {
+            UIView.animate(withDuration: 0.5) {
+                // translation calculus:
+                let translationX = -(self.view.bounds.width/2 + self.layoutView.bounds.width/2)
+                // the translation itself:
+                self.layoutView.transform = CGAffineTransform(translationX: translationX, y: 0)
+            } completion: { (success) in
+                self.shareImage()
+            }
+        }
+    }
+
+    // give the view its initial position :
     private func reverseTranslation() {
         UIView.animate(withDuration: 0.5) {
             self.layoutView.transform = .identity
         }
+    }
+
+    // MARK: - Alerts
+
+    private func successAlert() {
+        let alert = UIAlertController(title: "✓", message: "Success !", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+
+    private func accessAlert() {
+        let alertController = UIAlertController(title: "⚠︎", message: "We need access to your gallery", preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "Ok", style: .default, handler: { (action) in
+            PHPhotoLibrary.requestAuthorization { (status) in }
+        }))
+        present(alertController, animated: true, completion: nil)
+    }
+
+    private func needAccessAlert() {
+        let alertController = UIAlertController(title: "Authorization denied", message: "Go to Settings > Instagrid and grant access to your photos to continue", preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "Go to Settings", style: .default, handler: { (action) in
+            // allows to go directly in user's phone parameters:
+            if let settingsURL = URL(string: UIApplication.openSettingsURLString) {
+                UIApplication.shared.open(settingsURL)
+            }
+        }))
+        present(alertController, animated: true, completion: nil)
+    }
+}
+
+    // MARK: - ImagePicker management
+
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey:Any]) {
+        // check and unwrap the image chosen by the user:
+        guard let image = info[.originalImage] as? UIImage else {
+            return
+        }
+        // change the content mode of the button's image:
+        selectedButton?.imageView?.contentMode = .scaleAspectFill
+        // set chosen image in button's image:
+        selectedButton?.setImage(image, for: .normal)
+        // then selectedButton value is passed as nil to avoid mistakes:
+        selectedButton = nil
+        // dissmiss th picker:
+        picker.dismiss(animated: true)
     }
 }
